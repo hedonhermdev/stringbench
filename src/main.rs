d216e0d193315a9081cc2dbfc835b155c5a65d84
Tirth Jain <jaintirth24@gmail.com>
2021-07-27 20:13:00 +0530

    Benchmarking

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
-#![feature(test)]
 use bare_metal_modulo::*;
 use fast_tracer::{stats, svg};
 use lipsum::lipsum_words_from_seed;
 use num::traits::Pow;
 use rayon::prelude::*;
-use tracing::{Level,span};
 use std::time::{Instant, Duration};
 
 const K: u32 = 7907;
 const M: u32 = 256;
 
-fn timed<T>(body: impl FnOnce() -> T) -> (T, Duration) {
+fn timed<T>(body: impl FnOnce() -> T) -> (T, std::time::Duration) {
     let start = Instant::now();
     let result = body();
     let time_taken = start.elapsed();
     (result, time_taken)
 }
 
-fn string_match(haystack: &[u8], needle: &[u8], target_time: Duration) -> usize {
+fn string_match(haystack: &[u8], needle: &[u8], block_size: usize) -> usize {
     let weight = ModNum::new(M, K).pow(needle.len() as u32 - 1);
     let needle_hash = needle.iter().fold(ModNum::new(0, K), |old, &x| {
         old * M + ModNum::new(x as u32, K)
@@ -25,10 +23,8 @@ fn string_match(haystack: &[u8], needle: &[u8], target_time: Duration) -> usize 
 
     let windows = haystack.par_windows(needle.len()).enumerate();
 
-    let span = span!(Level::TRACE, "iter_fold");
-    let _guard = span.enter();
     windows
-        .adaptive(target_time)
+        .adaptive(Duration::from_nanos(5000))
         .scan(
             || None,
             |state, (index, win)| {
@@ -57,15 +53,22 @@ fn string_match(haystack: &[u8], needle: &[u8], target_time: Duration) -> usize 
 }
 
 fn main() {
-    let haystack = lipsum_words_from_seed(600_000, 0);
-    let needle = haystack.chars().take(300_000).collect::<String>();
+    let haystack = lipsum_words_from_seed(60_000_000, 0);
+    let needle = haystack.chars().take(30_000_000).collect::<String>();
+
     eprintln!("built haystack {}", haystack.len());
 
-    println!("scheme,num_threads,time_taken");
+    println!("scheme,block_size,time_taken");
 
-    stats(|| {
-        let (_count, time_taken) =
-        timed(|| string_match(haystack.as_bytes(), needle.as_bytes(), Duration::from_nanos(400000)));
-        println!("{},{},{}", "time_based", 4, time_taken.as_nanos());
-    });
+    for n in 1..=32 {
+        let tp = rayon::ThreadPoolBuilder::new().num_threads(n).build().expect("failed to build tp");
+        for _ in 0..10 {
+
+            tp.install(|| {
+                let (_count, time_taken) =
+                    timed(|| string_match(haystack.as_bytes(), needle.as_bytes(), 0));
+                    println!("{},{},{}", "time_based", tp.current_num_threads(), time_taken.as_nanos());
+            });
+        }
+    }
 }
